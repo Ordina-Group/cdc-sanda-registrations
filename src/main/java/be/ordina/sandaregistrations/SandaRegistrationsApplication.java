930158c046b8025fbd225391c6d265ed9d43a8bf
madiordina <mdudaeva@hziv.be>
2019-09-26 14:19:32 +0200

    First commit

---
+++ src/main/java/be/ordina/sandaregistrations/SandaRegistrationsApplication.java
@@ -0,0 +1,13 @@
+package be.ordina.sandaregistrations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandaRegistrationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandaRegistrationsApplication.class, args);
+	}
+
+}
